91d6815336e633462b5824f4e3bb9522068b338d
Jitgitbit <rowowolf@gmail.com>
2020-03-27 12:16:54 +0100

    laying the groundworks

---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,19 @@
+import React, {createContext, useState} from 'react'
+
+export const BookContext = createContext();
+
+export default function BookContextProvider(props) {
+  const[books,setBooks] = useState([
+    {title:'One flu', author:'Mack', id:1},
+    {title:'Black Pigeon speaks', author:'Jack', id:2},
+    {title:'Widow', author:'Muriel', id:3}
+  ])
+  const addBook = (title, artist) => {
+    setBooks([...books, {title:title, artist:artist}])
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
